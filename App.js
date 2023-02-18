977a2e6c1645d0f8ffee000d97beb2d2c0367310
Morov78 <98202937+Morov78@users.noreply.github.com>
2023-02-19 00:13:55 +0200

    clear code

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
 // import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  Dimensions,
-  Alert,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
 import LoginScreen from "./Screens/LoginScreen";
 import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
-  // const [dimensions, setdimensions] = useState(Dimensions.get("window").width);
-
-  // useEffect(() => {
-  //   const onChange = () => {
-  //     const width = Dimensions.get("window").width;
-  //     setdimensions(width);
-  //   };
-
-  //   Dimensions.addEventListener("change", onChange);
-  //   return () => {
-  //     Dimensions.removeEventListener("change", onChange);
-  //   };
-  // }, []);
-
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -45,8 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   image: {
     flex: 1,
